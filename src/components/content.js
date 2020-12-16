9d7b43f2b94434a75c409263dbc52f1a7aaca744
Riley Heeley (UG) <b9050713@newcastle.ac.uk>
2020-12-16 16:29:22 +0000

    first commit

---
+++ src/components/content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Content = ({ children }) => {
+  return (
+    <div class="my-32 sm:w-4/5 mx-auto">
+    {children}
+    </div>
+  )
+}
+
+export default Content;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,30 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Hero from "../components/hero"
+import Content from "../components/content"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+
+ <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Hero />
+
+    <Content>
+      <div class="flex">
+        <p class="flex-initial">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+
+        <p class="flex-initial">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </Content>
+
   </Layout>
+  
 )
 
 export default IndexPage
